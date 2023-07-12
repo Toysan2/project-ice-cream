eaeaf1e84be0a2c7e34661730304330253fad5e0
Astrix1234 <130665627+Astrix1234@users.noreply.github.com>
2023-07-12 18:28:19 +0200

    cerrected modal and button buy now

--- src/js/modal-buy.js
+++ src/js/modal-buy.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modalBuy-open]'),
+    openModalBtn2: document.querySelector('[modal-buynow-open]'),
     closeModalBtn: document.querySelector('[data-modalBuy-close]'),
     modal: document.querySelector('[data-modalBuy]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
